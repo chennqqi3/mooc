0cd480ad012e4c0c47d6fb057bab59cc19d427c4
mingshen <runtimeerror@outlook.com>
2017-09-03 15:41:37 +0800

    部署设置

--- apps/utils/email_send.py
+++ apps/utils/email_send.py
@@ -34,19 +34,19 @@ def send_register_email(email, send_type='register'):
         email_body = '请点击下面的链接激活你的账号：http://127.0.0.1:8000/active{}'.format(code)
         send_status = send_mail(email_title, email_body, EMAIL_FROM, [email])
         if send_status:
-            print('发送成功')
+            # print('发送成功')
     elif send_type == 'forget':
         email_title = '慕课在线重置密码链接'
         email_body = '请点击下面的链接重置你的账号：http://127.0.0.1:8000/reset/{}'.format(code)
         send_status = send_mail(email_title, email_body, EMAIL_FROM, [email])
         if send_status:
-            print('发送成功')
+            # print('发送成功')
     elif send_type == 'update_email':
         email_title = '慕课在线邮箱修改验证码'
         email_body = '你的邮箱验证号为：{}'.format(code)
         send_status = send_mail(email_title, email_body, EMAIL_FROM, [email])
         if send_status:
-            print('发送成功')
+            # print('发送成功')
 
 
 
