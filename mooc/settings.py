f890373440437cc9d7f9fa2c98388f5ce2d5f267
mingshen <runtimeerror@outlook.com>
2017-09-03 10:01:07 +0800

    部署设置

--- mooc/settings.py
+++ mooc/settings.py
@@ -137,10 +137,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
+
 
 # 邮件设置 #############################
 
@@ -154,7 +151,11 @@ EMAIL_FROM = 'tiedanqi@sohu.com'
 ########################################
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
+STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static').replace('\\', '/')
